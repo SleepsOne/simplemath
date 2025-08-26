13bbfb415f5f729438e713fd33ecf7be617f8153
SleepsOne <DucLH.B21CN034@stu.ptit.edu.vn>
2025-08-26 14:10:01 +0700

    v2.0.0: make Add generic using Number interface

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/SleepsOne/simplemath
 
 go 1.24.4
+
+require golang.org/x/exp v0.0.0-20250819193227-8b4c13bb791b
--- simplemath.go
+++ simplemath.go
@@ -1,8 +1,14 @@
-// Package simplemath provides basic math operations.
 package simplemath
 
-// Add takes two integers and returns their sum.
+import "golang.org/x/exp/constraints"
+
+// Number is a generic type that covers integers and floats.
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
+// Add returns the sum of two numbers of type Number.
 // Learn more about addition here: https://www.mathsisfun.com/numbers/addition.html
-func Add(a int, b int) int {
+func Add[T Number](a, b T) T {
 	return a + b
 }
