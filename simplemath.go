fa2d9f355e570b4dd8fc9096d5311147633e5e8d
SleepsOne <DucLH.B21CN034@stu.ptit.edu.vn>
2025-08-26 14:02:37 +0700

    v1.0.1: add godoc comments

--- simplemath.go
+++ simplemath.go
@@ -1,5 +1,8 @@
+// Package simplemath provides basic math operations.
 package simplemath
 
+// Add takes two integers and returns their sum.
+// Learn more about addition here: https://www.mathsisfun.com/numbers/addition.html
 func Add(a int, b int) int {
 	return a + b
 }
